8271a3d63f40897d3f8a18791532062de0a26786
abbasaman-says <abbaskhan462894162@gmail.com>
2026-09-14 15:51:02 +0500

    frontend change url

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "focused-empathy-production.up.railway.app",
-    // baseURL: "https://focused-empathy-production.up.railway.app/",
+    baseURL: "https://focused-empathy-production.up.railway.app",
     withCredentials: true
 });
 
